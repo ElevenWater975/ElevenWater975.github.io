2cff497ef202d2d9546f9f5fd521dc0fa1d5a228
ElevenWater975 <65044666+ElevenWater975@users.noreply.github.com>
2021-04-08 22:11:03 -0700

    Update index.html

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function text() {
     alert("This alert")
 }
+const listen = event => {
+    const p = document.createElement('p');
+    p.innerText = "The button worked!";
+    document.querySelector('#container').appendChild(p);
+  }
