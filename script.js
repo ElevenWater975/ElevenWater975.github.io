be11e59bdfc2620b945c4301644db29cecbc9e72
ElevenWater975 <65044666+ElevenWater975@users.noreply.github.com>
2021-04-08 20:14:15 -0700

    Update script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// this is the first time
-if (! localStorage.noFirstVisit) {
-    // show the element
-    // and do the animation you want
-    document.getElementById('first').style.display = 'block';
-
-    // check this flag for escaping this if block next time
-    localStorage.noFirstVisit = "1";
-}
