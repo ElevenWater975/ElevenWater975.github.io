c6d51f037b22705dc20fc3be80d4153f2cae394d
ElevenWater975 <65044666+ElevenWater975@users.noreply.github.com>
2021-04-14 19:17:26 -0700

    Update index.html

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function scrollUp() {
 if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
 myButton.style.display = "block";   
 } else {
-    myButton.style.display = "none; 
+    myButton.style.display = "none"; 
 }
 }
 
