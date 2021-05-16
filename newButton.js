c39d6ed35ff33ed9d47112e06f4ea2cad5866cbf
ElevenWater975 <65044666+ElevenWater975@users.noreply.github.com>
2021-05-16 16:29:46 -0700

    Delete newButton.js

--- newButton.js
+++
@@ -1,5 +0,0 @@
-const newButton = event => {
-    const para = document.createElement('p');
-     para.innerText = "This site is in v1.0.2-Alpha. It is not completely released. Just a pre-release";
-     document.querySelector('#anotherButton').appendChild(para);
-}
