2c207220368ddf120406d44c1661ced89e8ae047
ElevenWater975 <65044666+ElevenWater975@users.noreply.github.com>
2021-05-16 16:29:41 -0700

    Create script.js

---
+++ Buttons/script.js
@@ -0,0 +1,9 @@
+function text() {
+    alert("This alert")
+}
+const listen = event => {
+    const p = document.createElement('p');
+    p.innerText = "Well, except for this";
+    document.querySelector('#container').appendChild(p);
+  }
+ // This makes the button do something
