b79691a17f2507d1de1d9ca71871d375a2ceab36
ElevenWater975 <65044666+ElevenWater975@users.noreply.github.com>
2021-06-10 15:43:57 -0700

    Fix bug

--- BlockPro/BlockPro.js
+++ BlockPro/BlockPro.js
@@ -10,4 +10,22 @@ function closeNav() {
     document.getElementById("popoutNav").style.display = "none";
 }
 
-v
+function popoutSettings() {
+    document.getElementById('settings').style.display = "block";
+
+ 
+}
+
+function closeSettings() {
+    document.getElementById('settings').style.display = "none";
+}
+
+function darkMode() {
+    var darkTheme = document.body;
+    darkTheme.classList.toggle("darkMode");
+}
+
+function lightMode() {
+    var lightTheme = document.body;
+    lightTheme.classList.toggle("lightMode");
+}
