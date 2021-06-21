2b106ec36033a516909265807290229738b60145
ElevenWater975 <nje.eleven34@icloud.com>
2021-06-20 22:10:10 -0700

    Commit to main

---
+++ Updates.js
@@ -0,0 +1,9 @@
+function enableDarkMode() {
+    var element = document.body;
+    element.classList.toggle('darkmode');
+}
+// The function below is not working
+function showSettings() {
+document.getElementById('derk-mode').style.display = "block";
+}
+// why is it not working?
