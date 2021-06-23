92004c427215bd0694f3f59112d8894e84a0ba6b
ElevenWater975 <nje.eleven34@icloud.com>
2021-06-22 20:27:33 -0700

    Commit to main

---
+++ stickynotes.js
@@ -0,0 +1,6 @@
+function alertThenFunction() {
+    var bye = confirm('Welp cya! But are you sure you wanna leave?');
+    if (bye) {
+        alert("Alright cya!");
+    }
+// Another thanks to https://sebhastian.com/javascript-confirmation-yes-no/#:~:text=You%20can%20create%20a%20JavaScript,boolean%20value%20true%20or%20false%20. for the js confirm tutorial
