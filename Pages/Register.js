54380f10e49aabb4b502f87af08790eafa7298d9
ElevenWater975 <nyrangerzepeda21@outlook.com>
2021-06-30 21:25:43 -0700

    Commit to main

--- Pages/Register.js
+++ Pages/Register.js
@@ -1,3 +1,13 @@
 const appear = () => {
     document.getElementById("links").style.display = "block";
 }
+
+function blueMode() {
+    var blue = document.body;
+    blue.classList.toggle("bluemode");
+}
+
+function lightMode() {
+    var light = document.body;
+    light.classList.toggle("lightmode");
+}
