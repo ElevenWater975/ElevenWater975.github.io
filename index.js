0c77662cd7ce056f5d74d54b8fbfcc81119b32e1
ElevenWater975 <nyrangerzepeda21@outlook.com>
2021-07-05 21:28:07 -0700

    Commit to main

---
+++ index.js
@@ -0,0 +1,12 @@
+function buttonSupport() {
+    var x = location.href = "https://elevenwater975.github.io/support";
+}
+
+function buttonNewsletter() {
+    var y = location.href = "https://elevenwater975.github.io/newsletter"
+}
+
+function closeNav() {
+    document.getElementById("links").style.display = "none";
+    document.querySelector("links").className = "navClose";
+}
