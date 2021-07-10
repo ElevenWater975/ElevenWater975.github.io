1a5c69328bf76ddbea664bbfce1c82cdf4f512f5
ElevenWater975 <nyrangerzepeda21@outlook.com>
2021-07-09 20:40:09 -0700

    commit to main

--- index.js
+++ index.js
@@ -16,3 +16,32 @@ function openNav() {
     document.getElementById("main").style.marginLeft = "250px";
 }
 
+function swap() {
+    var x = document.getElementById("swapT");
+if (x.innerHTML === "If this page is not working properly, please contact nje.eleven34@icloud.com") {
+x.innerHTML = "Swapped text!";
+   newbutton = document.getElementById("anotherBtn");
+   window.onscroll = function() {scrollFunction()};
+}
+}
+       function soon() {
+           alert("Coming soon");
+       }
+
+       function newBtnFunc() {
+           document.body.scrollTop = 0;
+           document.documentElement.scrollTop = 0;
+       }
+
+
+     function easteregg() {
+         alert("A pointless easter egg")
+     }
+     function appear() {
+         document.getElementById("links").style.display = "block";
+     }
+
+     function textStuff() {
+         // Easter egg. Also I bet you are seeing this file through github so this egg may be easy.
+         document.getElementById("egg") = document.body.innerHTML.repalce("p", "hello im an easter egg");
+     }
---
+++ reach.js
@@ -0,0 +1,12 @@
+function emailFunction() {
+    document.addEventListener('click', function(){
+document.getElementById("emails").style.display='block';
+    
+    })
+}
+
+function discordFunction() {
+document.addEventListener('click', function(){
+    document.getElementById("discord").style.display='block';
+})
+}
