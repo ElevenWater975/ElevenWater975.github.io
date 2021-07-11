ad2711ce43d354174715b9e411030807095cf049
ElevenWater975 <nyrangerzepeda21@outlook.com>
2021-07-10 19:56:12 -0700

    Commit to main

--- reach.js
+++ reach.js
@@ -10,3 +10,14 @@ document.addEventListener('click', function(){
     document.getElementById("discord").style.display='block';
 })
 }
+
+function enableNav() {
+    document.getElementById("navbar").style.display = "block";
+}
+
+function closeNav() {
+    var close = document.getElementById("navbar");
+document.addEventListener('click', function(){
+    close.classList.add = "animation";
+})
+}
