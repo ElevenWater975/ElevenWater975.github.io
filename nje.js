8cf1a3f716d45368c5dff7e3f93b3a5559ddf894
ElevenWater975 <nyrangerzepeda21@outlook.com>
2021-07-19 21:05:35 -0700

    commit to main

--- nje.js
+++ nje.js
@@ -10,5 +10,18 @@ function notice() {
         alert("As I said many times. hol up real quick the feature isn't available.");
     } else if (count === 7) {
         alert("please go away, the feature isn't available.");
+    } else if (count === 10) {
+        alert("So you're just gonna keep clicking to see what happens huh?");
+    } else if (count === 11) {
+        alert("gullible");
     }
 }
+
+function darkMode() {
+    document.getElementById("darkmode").style.background = "black";
+    document.getElementById("darkmode").style.color = "white";
+    document.getElementById("external-links").style.background = "black";
+    document.getElementById("footer").style.background = "black";
+    document.getElementById("footer").style.padding = "50px"
+}
+document.getElementById("external-links").style.padding = "50px;"
