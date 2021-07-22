92e27d7dd82fc927f1f79beba06df3e3e57e235f
ElevenWater975 <nyrangerzepeda21@outlook.com>
2021-07-21 21:08:56 -0700

    commit to main

--- njenews.js
+++ njenews.js
@@ -1,5 +1,7 @@
 function openNav() {
-    document.getElementById("links").style.display = "block"
+    document.getElementById("links").style.display = "block";
+    document.getElementById("settingsbutton").style.float = "left";
+    document.getElementById("settingsbutton").style.left = "-0%";
 }
 
 function darkMode() {
@@ -20,3 +22,56 @@ function buttonColor() {
     button.classList.toggle("darkmodebutton");
 
 }
+
+function openSettings() {
+    var button = document.body;
+    document.getElementById("settings").style.display = "block";
+    if (document.getElementById("links").style.display === "block") {
+        alert("Please close the navigation / links bar");
+        document.getElementById("settings").style.display = "none";
+    } else if (document.getElementById("links").style.display === "none") {
+        document.getElementById("settings").style.display = "block";
+    }
+}
+
+function changeText() {
+var green = document.body; 
+green.classList.toggle("changeToGreen");
+}
+
+function moveImage() {
+    var move = document.body;
+    move = document.getElementById("NJE-Image").style.width = "150px";
+    move = documemt.getElementById("moveable").style.display = "block";
+}
+
+function closeNav() {
+    document.getElementById("links").style.display = "none";
+}
+
+function closeSettings() {
+    document.getElementById("settings").style.display = "none";
+}
+
+function skyBlueText() {
+    var skyblue = document.body;
+    
+
+    do {
+        document.getElementById("skybluetext").style.display = "block";
+        skyblue.classList.toggle("skytext");
+    } while (document.body.background === "black") {
+        do {
+            document.getElementById("skybluetext").style.display = "none";
+        skyblue.classList.remove("skytext");
+        } while (document.body.background === "white") {
+    
+        }
+    }
+}
+
+function buttonsappeared() {
+    document.addEventListener("click", function() {
+    document.getElementById("skybluetext").style.display = "block";
+}
+});
