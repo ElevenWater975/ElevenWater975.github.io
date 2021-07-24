812921e3b440de555fd2f29eaba6e4416804278b
ElevenWater975 <nyrangerzepeda21@outlook.com>
2021-07-23 20:53:26 -0700

    commit to main

---
+++ app.js
@@ -0,0 +1,33 @@
+function enableDark() {
+    var dark = document.body;
+    dark.classList.toggle('darkmode');
+}
+
+var emailEl = document.querySelector("#email-check");
+
+var usernameEl = document.querySelector("#username");
+
+var passwordEl = document.querySelector("#password");
+
+var passwordConfirmation = document.querySelector("#confirm-password");
+
+document.addEventListener("validate", function (e) {
+e.preventDefault();
+const isRequired = value === '' ? false : true;
+})
+
+const isPasswordSecure = (password) => {
+    const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+    return re.test(password);
+}
+
+const errorShow = (input, message) => {
+    const formField = input.parentElement; 
+}
+
+function notice() {
+    var x = document.getElementById("submit");
+    if (x.innerHTML === "Create new account!") {
+        x.innerHTML === "Not ready yet";
+    }
+}
