50e2717453b9559bb925fbad9b78dc9786e445a8
ElevenWater975 <nyrangerzepeda21@outlook.com>
2022-06-18 14:17:38 -0700

    Stuff

--- contact.php
+++ contact.php
@@ -1,4 +1,12 @@
-<?php
-echo '<h1>Thank you for contacting us!</h1>';
-echo '\n<p>We will try to get back to you within 24 hours</p>'
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
